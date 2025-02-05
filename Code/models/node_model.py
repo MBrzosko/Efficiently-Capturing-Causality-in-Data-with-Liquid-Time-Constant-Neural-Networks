ed03ef6a7039abc554a65edfaa44e51d848d1ce0
Michal Brzosko <michal.brzosko@googlemail.com>
2025-02-05 10:32:56 +0100

    Add typing and documentation of the NODE models.

--- Code/models/node_model.py
+++ Code/models/node_model.py
@@ -4,6 +4,12 @@ import torch.nn as nn
 import torchdiffeq
 
 
+"""
+Installation of the python package torchdiffeq is required (https://github.com/rtqichen/torchdiffeq)
+Additional information about the ODE code can be found here: https://github.com/YuliaRubanova/latent_ode
+"""
+
+
 class NeuralODEFunc(nn.Module):
     def __init__(
         self,
@@ -12,6 +18,14 @@ class NeuralODEFunc(nn.Module):
         output_dim,
         num_layers,
     ):
+        """
+        Feedforward neural network that parametrises the derivative of the input
+
+        :param input_dim: Input dimension.
+        :param hidden_dim: Number of neurons.
+        :param output_dim:Output dimension.
+        :param num_layers: Number of layers.
+        """
         super(NeuralODEFunc, self).__init__()
 
         self.net = None
@@ -26,42 +40,93 @@ class NeuralODEFunc(nn.Module):
 
         self.net = nn.Sequential(*layers)
 
-    def forward(self, t, y):
+    def forward(self, y: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the NeuralODEFunc.
+
+        :param y: Input.
+
+        :return: Derivative of the input.
+        """
 
         return self.net(y)
 
 
 class ODEBlock(nn.Module):
     def __init__(self, odefunc):
+        """
+        Contains the ODE solvers. Takes the NeuralODEFunc as an attribute.
+
+        :param odefunc: NeuralODEFunc.
+        """
         super(ODEBlock, self).__init__()
         self.odefunc = odefunc
 
-    def forward(self, y0, t):
+    def forward(self, y0: torch.Tensor, t: int) -> torch.Tensor:
+        """
+        ODE solver iteration.
+
+        :param y0: Initial condition.
+        :param t: Number of time steps.
+
+        :return: Predicted sequence.
+        """
 
         sol = torchdiffeq.odeint(self.odefunc, y0, t, rtol=1e-5, atol=1e-7)
         return sol
 
 
 class NeuralODE(nn.Module):
-    def __init__(self, odefunc, input_dim, output_dim):
+    def __init__(self, odefunc: nn.Module, input_dim: int, output_dim: int):
+        """
+        Full NODE Model.
+
+        :param odefunc: NeuralODEFunc.
+        :param input_dim: Input dimension.
+        :param output_dim: Output dimension.
+        """
         super(NeuralODE, self).__init__()
         self.odeblock = ODEBlock(odefunc)
         self.fc = nn.Linear(input_dim, output_dim)
 
-    def forward(self, y0, t):
+    def forward(self, y0: torch.Tensor, t: int) -> torch.Tensor:
+        """
+        Forward pass of the NODE.
+
+        :param y0: Initial condition.
+        :param t: Number of time steps.
+
+        :return: Predicted sequence
+        """
         out = self.odeblock(y0, t)
         out = self.fc(out)
         return out
 
 
 class EnforcedNeuralODE(nn.Module):
-    def __init__(self, odefunc, input_dim, output_dim):
+    def __init__(self, odefunc: nn.Module, input_dim: int, output_dim: int):
+        """
+        Full NODE Model with external forcing
+
+        :param odefunc: NeuralODEFunc.
+        :param input_dim: Input dimension.
+        :param output_dim: Output dimension.
+        """
         super(EnforcedNeuralODE, self).__init__()
         self.odeblock = ODEBlock(odefunc)
         self.t = torch.tensor(np.array([0.1]), dtype=torch.float32)
         self.fc = nn.Linear(input_dim, output_dim)
 
-    def forward(self, inputs, forcing, timespan):
+    def forward(self, inputs: torch.Tensor, forcing: torch.Tensor, timespan: int) -> torch.Tensor:
+        """
+        Forward pass of the NODE with external enforcing.
+
+        :param inputs: Initial condition.
+        :param forcing: External forcing.
+        :param timespan: Number of time steps.
+
+        :return: Predicted sequence.
+        """
 
         outputs = torch.empty((timespan, inputs.shape[0], inputs.shape[1]))
         outputs[0] = inputs
